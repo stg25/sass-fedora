b2f9605849971fee9deb85ae5d64a9e055c04272
stg25 <stefanoghizzi@yahoo.com>
2019-04-26 16:45:08 +0200

    Full site OK. ToDo: JS

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 
+function dropdownMenu() {
+  var dropdownOrder = $(".dropdownOrder");
+  dropdownOrder.toggleClass("dropdownOrderShow");
+
+}
+
 // Init function
 
 function init() {
+  var doc = $(document);
+
+  doc.on("click", "#dropdown", dropdownMenu)
+
 }
 
 $(document).ready(init);

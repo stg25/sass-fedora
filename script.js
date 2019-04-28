610e6444315cb7c4b55637619c2b535f0d98b165
stg25 <stefanoghizzi@yahoo.com>
2019-04-28 16:31:13 +0200

    Full site, ToDO: mediaquery

--- script.js
+++ script.js
@@ -1,16 +1,53 @@
 
-function dropdownMenu() {
-  var dropdownOrder = $(".dropdownOrder");
-  dropdownOrder.toggleClass("dropdownOrderShow");
+// shows dropdown menu
 
+function showDropdownMenu() {
+  var dropdownOrder = $(".dropdownOrder");
+  dropdownOrder.toggleClass("showDropdownOrder");
+}
+
+// show languages on flags
+
+function showLanguages() {
+  var me = $(this);
+  var finded = me.find("div.language");
+
+  finded.removeClass("language");
+  finded.addClass("showLanguage");
+}
+
+// hide languages on flags
+
+function hideLanguages() {
+  var me = $(this);
+  var finded = me.find("div.showLanguage");
+
+  finded.removeClass("showLanguage");
+  finded.addClass("language");
+}
+
+// first container carousel
+
+function changeImg() {
+  var container = $(".first-container");
+  container.toggleClass("img2");
 }
 
 // Init function
 
 function init() {
   var doc = $(document);
-
-  doc.on("click", "#dropdown", dropdownMenu)
+  // show dropdown
+  doc.on("click", "#dropdown", showDropdownMenu);
+  // show language
+  doc.on("mouseenter", ".single-country", showLanguages);
+  // hide language
+  doc.on("mouseleave", ".single-country", hideLanguages);
+  // img carousel every 10 seconds
+  setInterval(function () {
+    var container = $(".first-container");
+    container.toggleClass("img2");
+  }, 10000)
 
 }
 
